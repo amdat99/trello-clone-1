b307cb82c15e3539d5f4fb1d38967e1e6c989390
Aamir Adat <74106642+amdat99@users.noreply.github.com>
2022-01-28 15:26:29 +0000

    fix dependencies and commented out erroring code

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,54 +1,52 @@
 // Packages
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-import Loading from './Components/Loading.js'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Loading from "./Components/Loading.js";
 
 // Protected Route for Auth
-import ProtectedRoute from './ProtectedRoute.js';
+import ProtectedRoute from "./ProtectedRoute.js";
 
 // Context API
-import { UserContext } from './userContext';
-import { AuthContext } from './authContext';
+import { UserContext } from "./userContext";
+import { AuthContext } from "./authContext";
 
 // Page Components
-import Trello from './Trello';
-import Landing from './Landing';
+import Trello from "./Trello";
+import Landing from "./Landing";
 
 // CSS Imports
-import './App.css';
-import './Components/card.css';
+import "./App.css";
+import "./Components/card.css";
 
-function App( {history} ) {
+function App({ history }) {
   const [userId, setUserId] = useState("Default Value");
   const [authed, setAuthed] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      const loggedIn = localStorage.getItem('rememberMe') === 'true';
-      if (loggedIn){
-        setUserId(localStorage.getItem('userId'));
-        setAuthed(true);
-      }    
+    const loggedIn = localStorage.getItem("rememberMe") === "true";
+    if (loggedIn) {
+      setUserId(localStorage.getItem("userId"));
+      setAuthed(true);
+    }
 
-      setLoading(false);
-    },
-    []);
+    setLoading(false);
+  }, []);
 
-  
-  return (loading 
-    ?
-      <Loading />
-    :
-      <AuthContext.Provider value={{authed, setAuthed}}>
-      <UserContext.Provider value={{userId, setUserId}}>
-      <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={Landing}/>
-            <ProtectedRoute component={Trello}/>
-        </Switch>
-      </BrowserRouter>
+  return loading ? (
+    <Loading />
+  ) : (
+    <AuthContext.Provider value={{ authed, setAuthed }}>
+      <UserContext.Provider value={{ userId, setUserId }}>
+        <Trello />
+        {/* <BrowserRouter>
+          <Routes>
+            <Route path="/" element={Landing} />
+            <Route element={Trello} />
+          </Routes>
+        </BrowserRouter> */}
       </UserContext.Provider>
-      </AuthContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    password: "jackb1105",
-    host: "localhost",
-    port: 5432,
-    database: "trelloclone"
-})
+  host: "138.68.182.30",
+  user: "postgres",
+  password: "White43534423?ghhjhh",
+  port: 5432,
+  database: "trelloclone",
+});
 
 module.exports = pool;
